fb495ae8a8daa8518f50c699cde4120a5e2d52a0
Syth-1 <Syth49@gmail.com>
2024-07-05 16:41:50 +0100

    first commit!

---
+++ app.ts
@@ -0,0 +1,19 @@
+import { mkdir, rmdir } from 'node:fs/promises'
+import { sleep } from 'bun'
+
+const fileCount = 3
+const folder = "files/"
+
+if (true) { // set to false to see it successfully read files on subsequent runs.
+    await rmdir(folder, {recursive : true})
+    await sleep(2000) // visually see our directory be deleted.
+}
+
+await mkdir(folder, {recursive : true})
+
+for (let i = 0; i < fileCount; i++) { 
+    const file = `file-${i+1}.ts`
+    Bun.write(folder + file, `console.log("calling from ${file}")`)
+
+    await import(`@/${folder}${file}`)
+}
