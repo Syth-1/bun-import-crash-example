0f86bb4dc7a594feebd851e49936a6b412bf62e6
Syth-1 <Syth49@gmail.com>
2024-12-02 00:50:26 +0000

    added constant for filePath

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import { mkdir, rm } from 'node:fs/promises'
 import { sleep } from 'bun'
 
 const fileCount = 3
-const folder = "files/"
+const folder = "files"
 const importPathOverride = true // set to false to see it work as intended
 
 if (true) { // set to false to see bun successfully read files on subsequent runs.
@@ -11,12 +11,12 @@ if (true) { // set to false to see bun successfully read files on subsequent run
 }
 
 await mkdir(folder, {recursive : true})
-
 const importPrefix = importPathOverride ? '@' : '.'
 
 for (let i = 0; i < fileCount; i++) { 
     const file = `file-${i+1}.ts`
-    await Bun.write(folder + file, `console.log("calling from ${file}")`)
+    const filePath = `${folder}/${file}`
 
-    await import(`${importPrefix}/${folder}${file}`)
+    await Bun.write(filePath, `console.log("calling from ${file}")`)
+    await import(`${importPrefix}/${filePath}`)
 }
